ce426b4655469662b4bed821df8c5793f2d2dca6
Salil Kapur <salilkapur93@gmail.com>
2019-01-31 18:04:19 -0800

    Adding gitignore Implemented first API to authenticate users

---
+++ server/server.py
@@ -0,0 +1,46 @@
+from flask import Flask, request, jsonify
+from pymysql import cursors, connect
+
+app = Flask(__name__)
+db_conn = None # For connection to sql database
+
+def connect_database():
+    connection = connect(host='localhost',
+                         user='salil',
+                         password='s',
+                         db='CRM',
+                         cursorclass=cursors.DictCursor)
+
+    return connection
+
+@app.route('/')
+def root():
+    return 'Welcome to CRM'
+
+@app.route('/auth_user', methods=['GET'])
+def authenticate_user():
+    global db_conn
+
+    username = request.args.get('username')
+    password = request.args.get('password')
+    
+    if db_conn is None:
+        db_conn = connect_database()
+
+    with db_conn.cursor() as cursor:
+        query = "SELECT * FROM users WHERE username=%s AND password=%s"
+        cursor.execute(query, (username, password))
+        result = cursor.fetchone()
+
+        if result is not None and result['is_active'] == 1:
+            return jsonify(result)
+        else:
+            return jsonify("false")
+
+def main():
+    global db_conn
+    # Connect to database
+    db_conn = connect_database()
+
+if __name__ == '__main__':
+    main()
